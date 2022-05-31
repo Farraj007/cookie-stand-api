06242358135ff036c05c6276c3fad5a0b3b8142c
Barham Farraj <barhamfarraj@icloud.com>
2022-05-31 21:51:24 +0300

    cookie-stand

---
+++ cookie_stands/views.py
@@ -0,0 +1,18 @@
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .models import CookieStand
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CookieSerializer
+
+
+class CookieStandList(ListCreateAPIView):
+    queryset = CookieStand.objects.all()
+    serializer_class = CookieSerializer
+
+
+class CookieStandDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = CookieStand.objects.all()
+    serializer_class = CookieSerializer
--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ env = environ.Env(
     DATABASE_PORT=(int, 5432),
 )
 
+
 environ.Env.read_env()
 
 ENVIRONMENT = env.str("ENVIRONMENT")
@@ -60,7 +61,7 @@ INSTALLED_APPS = [
     "corsheaders",
     # local
     "accounts",
-    "things",
+    "cookie_stands",
 ]
 
 MIDDLEWARE = [
@@ -166,5 +167,10 @@ REST_FRAMEWORK = {
     ],
 }
 
+# CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
+# CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+# CSRF_TRUSTED_ORIGINS = tuple(env.list("ALLOWED_ORIGINS"))
+
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+CSRF_COOKIE_SECURE=True
