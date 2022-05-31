c122c80393f8ab586a6a8f48d4b0eff8c91de28f
Barham Farraj <barhamfarraj@icloud.com>
2022-05-31 23:16:13 +0300

    cookie-stand

--- cookie_stands/views.py
+++ cookie_stands/views.py
@@ -5,14 +5,12 @@ from rest_framework.generics import (
 from .models import CookieStand
 from .permissions import IsOwnerOrReadOnly
 from .serializers import CookieSerializer
-from django.views.decorators.csrf import csrf_protect
+# from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
 class CookieStandList(ListCreateAPIView):
     queryset = CookieStand.objects.all()
     serializer_class = CookieSerializer
 
-@csrf_protect
 class CookieStandDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     queryset = CookieStand.objects.all()
